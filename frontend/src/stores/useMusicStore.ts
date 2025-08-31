70af141f85b82af5e35a2052be260f9b82ff0035
muh-habeeb <muhammadhabeebrahmam@gmail.com>
2025-08-31 17:14:17 +0530

    datatypes added

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -1,8 +1,9 @@
 import { axiosInstance } from "@/lib/axios"
+import type { Album, Song } from "@/types"
 import { create } from "zustand"
 interface MusicStore {
-    songs: any[],
-    albums: any[],
+    songs: Song[],
+    albums: Album[],
     isLoading: boolean,
     error: string | null,
     fetchAlbums: () => Promise<void>
